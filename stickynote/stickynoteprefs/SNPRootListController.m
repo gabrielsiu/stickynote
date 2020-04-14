282a16ca0e32d4fd159698863282b9cd5804d91a
Gabriel Siu <gabrielsiu831@gmail.com>
2020-04-14 16:14:08 -0400

    Started adding preferences

---
+++ stickynote/stickynoteprefs/SNPRootListController.m
@@ -0,0 +1,13 @@
+#include "SNPRootListController.h"
+
+@implementation SNPRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
